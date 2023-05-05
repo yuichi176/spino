0b3d3a45c143c3786cde0bffbceebe5aefae46d5
Yuichi Sugiyama <ozwald176@gmail.com>
2023-05-05 10:41:35 +0900

    [fix] build setting

--- spino-be/build.gradle.kts
+++ spino-be/build.gradle.kts
@@ -12,6 +12,11 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+tasks.jar {
+	// xxx-plain.jarを生成しない設定
+	enabled = false
+}
+
 repositories {
 	mavenCentral()
 }
