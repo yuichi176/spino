2f860fdeb6aad7d8e3b92fcdb2853471c9177e78
Yuichi Sugiyama <ozwald176@gmail.com>
2023-05-15 22:45:29 +0900

    [add] meta info

--- spino-web/src/pages/_document.tsx
+++ spino-web/src/pages/_document.tsx
@@ -2,9 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
           <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta charSet='utf-8' />
+          <meta name='description' content='毎日生き物を紹介するWebアプリケーションです。' />
+          <meta name="keywords" content="動物, 生き物, 紹介, Webアプリケーション" />
+          <title>Today's Wildlife</title>
       </Head>
       <body>
         <Main />
