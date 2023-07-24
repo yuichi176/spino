00a5fdc768bf842a1508237cd9cd0504f11af5f8
Yuichi Sugiyama <ozwald176@gmail.com>
2023-07-24 22:40:31 +0900

    Update UI

--- spino-web/src/pages/index.tsx
+++ spino-web/src/pages/index.tsx
@@ -58,8 +58,8 @@ export default function Home() {
         <div className="mt-[200px] mx-auto pl-[5px] w-[60%] min-w-[250px] font-kosugi-maru">
           <h1 className="text-center text-[22px] mb-[5px]"><span className="text-highlight-blue">{!error ? wildlife?.name: "レッサーパンダ"}</span></h1>
           <p className="text-center text-[14px] text-[#1C77A6] mb-[20px] underline underline-offset-4">{!error ? wildlife?.habitat: "ヒマラヤ地域、中国、ネパール、インド"}</p>
-          <p className="text-[14px] text-[#164681] mb-[15px]">{!error ? wildlife?.description: "レッサーパンダは、小型哺乳動物であり、外見はクマとネコに似ています。彼らは主に竹を食べ、葉、果物、昆虫、鳥卵も食べます。彼らは木の上で生活し、しばしば昼間は寝ています。彼らは非常にかわいらしい外見で人気がありますが、野生種は絶滅が危惧されています。"}</p>
-          <p className="text-[14px] text-[#164681]">{!error ? wildlife?.trivia: "レッサーパンダは、竹を消化するために特別な細菌を持っています。"}</p>
+          <p className="text-[14px] text-[#123866] mb-[15px]">{!error ? wildlife?.description: "レッサーパンダは、小型哺乳動物であり、外見はクマとネコに似ています。彼らは主に竹を食べ、葉、果物、昆虫、鳥卵も食べます。彼らは木の上で生活し、しばしば昼間は寝ています。彼らは非常にかわいらしい外見で人気がありますが、野生種は絶滅が危惧されています。"}</p>
+          <p className="text-[14px] text-[#123866]">{!error ? wildlife?.trivia: "レッサーパンダは、竹を消化するために特別な細菌を持っています。"}</p>
         </div>
         <div className="absolute top-[610px] left-1/2 translate-x-[-50%] flex items-center font-kosugi-maru">
           <div>
@@ -72,7 +72,7 @@ export default function Home() {
           <div className="flex flex-col justify-between items-center w-[90px]">
             <a href ={`https://ja.wikipedia.org/wiki/${!error ? wildlife?.name : "レッサーパンダ"}`} target="_blank" rel="noopener noreferrer">
               <img src="/images/ty-icon1.png" alt="today's wildlife icon" className="w-[40px] mx-auto" />
-              <p className="text-[10px] underline underline-offset-2 decoration-2 decoration-[#83BD9C]">Who am I?</p>
+              <p className="text-[10px] text-[#123866] tracking-tight mt-[-3px] underline underline-offset-2 decoration-2 decoration-[#83BD9C]">check wiki</p>
             </a>
           </div>
           <div>
