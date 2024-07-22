7dae0726b6342319ed6aaa270cff14cc79fe0ac9
Yuichi Sugiyama <ozwald176@gmail.com>
2024-07-22 19:51:37 +0900

    Add mock data

--- spino-web/src/config/env.ts
+++ spino-web/src/config/env.ts
@@ -5,6 +5,7 @@ interface Env {
   BFF_BASE_DOMAIN?: string
   BE_PROTOCOL?: string
   BE_BASE_DOMAIN?: string
+  USE_MOCK_DATA?: boolean
 }
 
 export const env = {
@@ -12,4 +13,5 @@ export const env = {
   BFF_BASE_DOMAIN: process.env.NEXT_PUBLIC_BFF_BASE_DOMAIN || 'localhost:3000',
   BE_PROTOCOL: process.env.NEXT_PUBLIC_BE_PROTOCOL || 'http',
   BE_BASE_DOMAIN: process.env.NEXT_PUBLIC_BE_BASE_DOMAIN || 'localhost:8080',
+  USE_MOCK_DATA: process.env.USE_MOCK_DATA === 'true',
 } as Env
--- spino-web/src/pages/api/wildlifes.ts
+++ spino-web/src/pages/api/wildlifes.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { env } from '@/config/env'
 import { wildlifeClient } from '@/libs/api-client/wildlifeClient'
 
 export default async function wildLifeHandler(req: NextApiRequest, res: NextApiResponse) {
@@ -7,8 +8,12 @@ export default async function wildLifeHandler(req: NextApiRequest, res: NextApiR
     case 'GET':
       try {
         const { date } = req.query
-        const { data: wildlife } = await wildlifeClient.getWildlife(date as string)
-        res.status(200).send(wildlife)
+        if (env.USE_MOCK_DATA) {
+          res.status(200).send(mockWildlife)
+        } else {
+          const { data: wildlife } = await wildlifeClient.getWildlife(date as string)
+          res.status(200).send(wildlife)
+        }
       } catch (error) {
         console.error(`fail to get collections:${(error as Error).message}`)
         console.error(error)
@@ -16,6 +21,15 @@ export default async function wildLifeHandler(req: NextApiRequest, res: NextApiR
       }
       break
     default:
-      res.status(405).end(`Method ${method} Not Allowed`)
+      res.status(405).send(`Method ${method} Not Allowed`)
   }
+}
+
+const mockWildlife = {
+  name: 'レッサーパンダ',
+  habitat: 'ヒマラヤ地域、中国、ネパール、インド',
+  description:
+    'レッサーパンダは、小型哺乳動物であり、外見はクマとネコに似ています。彼らは主に竹を食べ、葉、果物、昆虫、鳥卵も食べます。彼らは木の上で生活し、しばしば昼間は寝ています。彼らは非常にかわいらしい外見で人気がありますが、野生種は絶滅が危惧されています。',
+  trivia: 'レッサーパンダは、竹を消化するために特別な細菌を持っています。',
+  createdAt: 'レッサーパンダは、竹を消化するために特別な細菌を持っています。2024-07-23',
 }
